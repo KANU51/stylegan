e6633f57a279ae2348e4ce35c31cab193d875dfd
Giyoung Jeon <giyoung@unist.ac.kr>
2019-09-02 15:39:00 +0900

    define new networks with masked hidden and placeholder for mask parameter

--- training/networks_stylegan.py
+++ training/networks_stylegan.py
@@ -379,6 +379,290 @@ def G_style(
     return tf.identity(images_out, name='images_out')
 
 #----------------------------------------------------------------------------
+# Style-based generator with Bernoulli masks.
+# Composed of two sub-networks (G_mapping and G_synthesis) that are defined below.
+
+def G_style_masked(
+    latents_in,                                     # First input: Latent vectors (Z) [minibatch, latent_size].
+    labels_in,                                      # Second input: Conditioning labels [minibatch, label_size].
+    truncation_psi          = 0.7,                  # Style strength multiplier for the truncation trick. None = disable.
+    truncation_cutoff       = 8,                    # Number of layers for which to apply the truncation trick. None = disable.
+    truncation_psi_val      = None,                 # Value for truncation_psi to use during validation.
+    truncation_cutoff_val   = None,                 # Value for truncation_cutoff to use during validation.
+    dlatent_avg_beta        = 0.995,                # Decay for tracking the moving average of W during training. None = disable.
+    style_mixing_prob       = 0.9,                  # Probability of mixing styles during training. None = disable.
+    is_training             = False,                # Network is under training? Enables and disables specific features.
+    is_validation           = False,                # Network is under validation? Chooses which value to use for truncation_psi.
+    is_template_graph       = False,                # True = template graph constructed by the Network class, False = actual evaluation.
+    components              = dnnlib.EasyDict(),    # Container for sub-networks. Retained between calls.
+    **kwargs):                                      # Arguments for sub-networks (G_mapping and G_synthesis).
+
+    # Validate arguments.
+    assert not is_training or not is_validation
+    assert isinstance(components, dnnlib.EasyDict)
+    if is_validation:
+        truncation_psi = truncation_psi_val
+        truncation_cutoff = truncation_cutoff_val
+    if is_training or (truncation_psi is not None and not tflib.is_tf_expression(truncation_psi) and truncation_psi == 1):
+        truncation_psi = None
+    if is_training or (truncation_cutoff is not None and not tflib.is_tf_expression(truncation_cutoff) and truncation_cutoff <= 0):
+        truncation_cutoff = None
+    if not is_training or (dlatent_avg_beta is not None and not tflib.is_tf_expression(dlatent_avg_beta) and dlatent_avg_beta == 1):
+        dlatent_avg_beta = None
+    if not is_training or (style_mixing_prob is not None and not tflib.is_tf_expression(style_mixing_prob) and style_mixing_prob <= 0):
+        style_mixing_prob = None
+
+    # Setup components.
+    if 'synthesis' not in components:
+        components.synthesis = tflib.Network('G_synthesis', func_name=G_synthesis_mask, **kwargs)
+    num_layers = components.synthesis.input_shape[1]
+    dlatent_size = components.synthesis.input_shape[2]
+    if 'mapping' not in components:
+        components.mapping = tflib.Network('G_mapping', func_name=G_mapping_mask, dlatent_broadcast=num_layers, **kwargs)
+
+    # Setup variables.
+    lod_in = tf.get_variable('lod', initializer=np.float32(0), trainable=False)
+    dlatent_avg = tf.get_variable('dlatent_avg', shape=[dlatent_size], initializer=tf.initializers.zeros(), trainable=False)
+
+    # Evaluate mapping network.
+    dlatents = components.mapping.get_output_for(latents_in, labels_in, **kwargs)
+
+    # Update moving average of W.
+    if dlatent_avg_beta is not None:
+        with tf.variable_scope('DlatentAvg'):
+            batch_avg = tf.reduce_mean(dlatents[:, 0], axis=0)
+            update_op = tf.assign(dlatent_avg, tflib.lerp(batch_avg, dlatent_avg, dlatent_avg_beta))
+            with tf.control_dependencies([update_op]):
+                dlatents = tf.identity(dlatents)
+
+    # Perform style mixing regularization.
+    if style_mixing_prob is not None:
+        with tf.name_scope('StyleMix'):
+            latents2 = tf.random_normal(tf.shape(latents_in))
+            dlatents2 = components.mapping.get_output_for(latents2, labels_in, **kwargs)
+            layer_idx = np.arange(num_layers)[np.newaxis, :, np.newaxis]
+            cur_layers = num_layers - tf.cast(lod_in, tf.int32) * 2
+            mixing_cutoff = tf.cond(
+                tf.random_uniform([], 0.0, 1.0) < style_mixing_prob,
+                lambda: tf.random_uniform([], 1, cur_layers, dtype=tf.int32),
+                lambda: cur_layers)
+            dlatents = tf.where(tf.broadcast_to(layer_idx < mixing_cutoff, tf.shape(dlatents)), dlatents, dlatents2)
+
+    # Apply truncation trick.
+    if truncation_psi is not None and truncation_cutoff is not None:
+        with tf.variable_scope('Truncation'):
+            layer_idx = np.arange(num_layers)[np.newaxis, :, np.newaxis]
+            ones = np.ones(layer_idx.shape, dtype=np.float32)
+            coefs = tf.where(layer_idx < truncation_cutoff, truncation_psi * ones, ones)
+            dlatents = tflib.lerp(dlatent_avg, dlatents, coefs)
+
+    # Evaluate synthesis network.
+    with tf.control_dependencies([tf.assign(components.synthesis.find_var('lod'), lod_in)]):
+        images_out = components.synthesis.get_output_for(dlatents, force_clean_graph=is_template_graph, **kwargs)
+    return tf.identity(images_out, name='images_out')
+
+def mask(x, pixelwise=True, use_placeholder=True):
+    if use_placeholder=True:
+        if pixelwise:
+            p = tf.placeholder_with_default(tf.constant(np.ones(shape=[1]+x.shape.as_list()[1:], dtype=np.float32)), 
+                                            shape=x.shape, name='MaskParams')
+        else:
+            p = tf.placeholder_with_default(tf.constant(np.ones(shape=[1,x.shape.as_list()[1]], dtype=np.float32)), 
+                                            shape=x.shape[0:2], name='MaskParams')
+            p = p[:,:,np.newaxis,np.newaxis]
+    else:
+            p = tf.get_variable(name='MaskParams', 
+                    shape=[1]+x.shape.as_list()[1:],
+                    dtype=np.float32,
+                    initializer=tf.ones_initializer)
+    u = tf.random.uniform(tf.shape(p),0,1)
+    z = tf.sigmoid(1/0.1*(tf.log(p+1e-7) - tf.log(1-p+1e-7) + tf.log(u+1e-7) - tf.log(1-u+1e-7)),
+                name='MaskSamples')
+    masked_x = tf.multiply(x, z, name='MaskedOutput')
+    return masked_x
+
+#----------------------------------------------------------------------------
+# Mapping network used in the StyleGAN paper.
+
+def G_mapping_mask(
+    latents_in,                             # First input: Latent vectors (Z) [minibatch, latent_size].
+    labels_in,                              # Second input: Conditioning labels [minibatch, label_size].
+    latent_size             = 512,          # Latent vector (Z) dimensionality.
+    label_size              = 0,            # Label dimensionality, 0 if no labels.
+    dlatent_size            = 512,          # Disentangled latent (W) dimensionality.
+    dlatent_broadcast       = None,         # Output disentangled latent (W) as [minibatch, dlatent_size] or [minibatch, dlatent_broadcast, dlatent_size].
+    mapping_layers          = 8,            # Number of mapping layers.
+    mapping_fmaps           = 512,          # Number of activations in the mapping layers.
+    mapping_lrmul           = 0.01,         # Learning rate multiplier for the mapping layers.
+    mapping_nonlinearity    = 'lrelu',      # Activation function: 'relu', 'lrelu'.
+    use_wscale              = True,         # Enable equalized learning rate?
+    normalize_latents       = True,         # Normalize latent vectors (Z) before feeding them to the mapping layers?
+    dtype                   = 'float32',    # Data type to use for activations and outputs.
+    **_kwargs):                             # Ignore unrecognized keyword args.
+
+    act, gain = {'relu': (tf.nn.relu, np.sqrt(2)), 'lrelu': (leaky_relu, np.sqrt(2))}[mapping_nonlinearity]
+
+    # Inputs.
+    latents_in.set_shape([None, latent_size])
+    labels_in.set_shape([None, label_size])
+    latents_in = tf.cast(latents_in, dtype)
+    labels_in = tf.cast(labels_in, dtype)
+    x = latents_in
+
+    # Embed labels and concatenate them with latents.
+    if label_size:
+        with tf.variable_scope('LabelConcat'):
+            w = tf.get_variable('weight', shape=[label_size, latent_size], initializer=tf.initializers.random_normal())
+            y = tf.matmul(labels_in, tf.cast(w, dtype))
+            x = tf.concat([x, y], axis=1)
+
+    # Normalize latents.
+    if normalize_latents:
+        x = pixel_norm(x)
+
+    # Mapping layers.
+    for layer_idx in range(mapping_layers):
+        with tf.variable_scope('Dense%d' % layer_idx):
+            fmaps = dlatent_size if layer_idx == mapping_layers - 1 else mapping_fmaps
+            x = dense(x, fmaps=fmaps, gain=gain, use_wscale=use_wscale, lrmul=mapping_lrmul)
+            x = apply_bias(x, lrmul=mapping_lrmul)
+            x = mask(act(x))
+
+    # Broadcast.
+    if dlatent_broadcast is not None:
+        with tf.variable_scope('Broadcast'):
+            x = tf.tile(x[:, np.newaxis], [1, dlatent_broadcast, 1])
+
+    # Output.
+    assert x.dtype == tf.as_dtype(dtype)
+    return tf.identity(x, name='dlatents_out')
+
+#----------------------------------------------------------------------------
+# Synthesis network used in the StyleGAN paper.
+
+def G_synthesis_mask(
+    dlatents_in,                        # Input: Disentangled latents (W) [minibatch, num_layers, dlatent_size].
+    dlatent_size        = 512,          # Disentangled latent (W) dimensionality.
+    num_channels        = 3,            # Number of output color channels.
+    resolution          = 1024,         # Output resolution.
+    fmap_base           = 8192,         # Overall multiplier for the number of feature maps.
+    fmap_decay          = 1.0,          # log2 feature map reduction when doubling the resolution.
+    fmap_max            = 512,          # Maximum number of feature maps in any layer.
+    use_styles          = True,         # Enable style inputs?
+    const_input_layer   = True,         # First layer is a learned constant?
+    use_noise           = True,         # Enable noise inputs?
+    randomize_noise     = True,         # True = randomize noise inputs every time (non-deterministic), False = read noise inputs from variables.
+    nonlinearity        = 'lrelu',      # Activation function: 'relu', 'lrelu'
+    use_wscale          = True,         # Enable equalized learning rate?
+    use_pixel_norm      = False,        # Enable pixelwise feature vector normalization?
+    use_instance_norm   = True,         # Enable instance normalization?
+    dtype               = 'float32',    # Data type to use for activations and outputs.
+    fused_scale         = 'auto',       # True = fused convolution + scaling, False = separate ops, 'auto' = decide automatically.
+    blur_filter         = [1,2,1],      # Low-pass filter to apply when resampling activations. None = no filtering.
+    structure           = 'auto',       # 'fixed' = no progressive growing, 'linear' = human-readable, 'recursive' = efficient, 'auto' = select automatically.
+    is_template_graph   = False,        # True = template graph constructed by the Network class, False = actual evaluation.
+    force_clean_graph   = False,        # True = construct a clean graph that looks nice in TensorBoard, False = default behavior.
+    **_kwargs):                         # Ignore unrecognized keyword args.
+
+    resolution_log2 = int(np.log2(resolution))
+    assert resolution == 2**resolution_log2 and resolution >= 4
+    def nf(stage): return min(int(fmap_base / (2.0 ** (stage * fmap_decay))), fmap_max)
+    def blur(x): return blur2d(x, blur_filter) if blur_filter else x
+    if is_template_graph: force_clean_graph = True
+    if force_clean_graph: randomize_noise = False
+    if structure == 'auto': structure = 'linear' if force_clean_graph else 'recursive'
+    act, gain = {'relu': (tf.nn.relu, np.sqrt(2)), 'lrelu': (leaky_relu, np.sqrt(2))}[nonlinearity]
+    num_layers = resolution_log2 * 2 - 2
+    num_styles = num_layers if use_styles else 1
+    images_out = None
+
+    # Primary inputs.
+    dlatents_in.set_shape([None, num_styles, dlatent_size])
+    dlatents_in = tf.cast(dlatents_in, dtype)
+    lod_in = tf.cast(tf.get_variable('lod', initializer=np.float32(0), trainable=False), dtype)
+
+    # Noise inputs.
+    noise_inputs = []
+    if use_noise:
+        for layer_idx in range(num_layers):
+            res = layer_idx // 2 + 2
+            shape = [1, use_noise, 2**res, 2**res]
+            noise_inputs.append(tf.get_variable('noise%d' % layer_idx, shape=shape, initializer=tf.initializers.random_normal(), trainable=False))
+
+    # Things to do at the end of each layer.
+    def layer_epilogue(x, layer_idx):
+        if use_noise:
+            x = apply_noise(x, noise_inputs[layer_idx], randomize_noise=randomize_noise)
+        x = apply_bias(x)
+        x = act(x)
+        if use_pixel_norm:
+            x = pixel_norm(x)
+        if use_instance_norm:
+            x = instance_norm(x)
+        if use_styles:
+            x = style_mod(x, dlatents_in[:, layer_idx], use_wscale=use_wscale)
+        return mask(x)
+
+    # Early layers.
+    with tf.variable_scope('4x4'):
+        if const_input_layer:
+            with tf.variable_scope('Const'):
+                x = tf.get_variable('const', shape=[1, nf(1), 4, 4], initializer=tf.initializers.ones())
+                x = layer_epilogue(tf.tile(tf.cast(x, dtype), [tf.shape(dlatents_in)[0], 1, 1, 1]), 0)
+        else:
+            with tf.variable_scope('Dense'):
+                x = dense(dlatents_in[:, 0], fmaps=nf(1)*16, gain=gain/4, use_wscale=use_wscale) # tweak gain to match the official implementation of Progressing GAN
+                x = layer_epilogue(tf.reshape(x, [-1, nf(1), 4, 4]), 0)
+        with tf.variable_scope('Conv'):
+            x = layer_epilogue(conv2d(x, fmaps=nf(1), kernel=3, gain=gain, use_wscale=use_wscale), 1)
+
+    # Building blocks for remaining layers.
+    def block(res, x): # res = 3..resolution_log2
+        with tf.variable_scope('%dx%d' % (2**res, 2**res)):
+            with tf.variable_scope('Conv0_up'):
+                x = layer_epilogue(blur(upscale2d_conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale, fused_scale=fused_scale)), res*2-4)
+            with tf.variable_scope('Conv1'):
+                x = layer_epilogue(conv2d(x, fmaps=nf(res-1), kernel=3, gain=gain, use_wscale=use_wscale), res*2-3)
+            return x
+    def torgb(res, x): # res = 2..resolution_log2
+        lod = resolution_log2 - res
+        with tf.variable_scope('ToRGB_lod%d' % lod):
+            return apply_bias(conv2d(x, fmaps=num_channels, kernel=1, gain=1, use_wscale=use_wscale))
+
+    # Fixed structure: simple and efficient, but does not support progressive growing.
+    if structure == 'fixed':
+        for res in range(3, resolution_log2 + 1):
+            x = block(res, x)
+        images_out = torgb(resolution_log2, x)
+
+    # Linear structure: simple but inefficient.
+    if structure == 'linear':
+        images_out = torgb(2, x)
+        for res in range(3, resolution_log2 + 1):
+            lod = resolution_log2 - res
+            x = block(res, x)
+            img = torgb(res, x)
+            images_out = upscale2d(images_out)
+            with tf.variable_scope('Grow_lod%d' % lod):
+                images_out = tflib.lerp_clip(img, images_out, lod_in - lod)
+
+    # Recursive structure: complex but efficient.
+    if structure == 'recursive':
+        def cset(cur_lambda, new_cond, new_lambda):
+            return lambda: tf.cond(new_cond, new_lambda, cur_lambda)
+        def grow(x, res, lod):
+            y = block(res, x)
+            img = lambda: upscale2d(torgb(res, y), 2**lod)
+            img = cset(img, (lod_in > lod), lambda: upscale2d(tflib.lerp(torgb(res, y), upscale2d(torgb(res - 1, x)), lod_in - lod), 2**lod))
+            if lod > 0: img = cset(img, (lod_in < lod), lambda: grow(y, res + 1, lod - 1))
+            return img()
+        images_out = grow(x, 3, resolution_log2 - 3)
+
+    assert images_out.dtype == tf.as_dtype(dtype)
+    return tf.identity(images_out, name='images_out')
+
+
+#----------------------------------------------------------------------------
 # Mapping network used in the StyleGAN paper.
 
 def G_mapping(
